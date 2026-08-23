201bf7bc1c006319568b2e7a5f59f718922f2e73
Shiva Deshpande <deshps11@mcmaster.ca>
2026-08-23 12:52:59 -0400

    connect frontend to deployed backend

--- api/main.py
+++ api/main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-origins = ["http://localhost:5173", "https://dna-decoding-full-stack.vercel.app/"]
+origins = ["http://localhost:5173", "https://dna-decoding-full-stack-dzq6ushlw-kangaroo0726s-projects.vercel.app/"]
 
 app.add_middleware(
     CORSMiddleware,
